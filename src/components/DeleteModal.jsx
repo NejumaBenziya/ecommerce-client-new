011e8dd8e1e48924c7dbd70d068f4fa3d18751fd
NejumaBenziya <sulfinejuma@gmail.com>
2026-04-10 22:42:28 +0530

    push origin

--- src/components/DeleteModal.jsx
+++ src/components/DeleteModal.jsx
@@ -8,7 +8,7 @@ const DeleteModal = ({ isOpen, onClose, onConfirm, itemName }) => {
       <div className="bg-white rounded-xl shadow-lg w-full max-w-md p-6">
         <h2 className="text-xl font-bold text-center mb-4">Confirm Delete</h2>
         <p className="text-center text-gray-600 mb-6">
-          Are you sure you want to delete{" "}
+          Are you sure you want to delete ?{" "}
           <span className="font-semibold">{itemName}</span>?  
           
         </p>
